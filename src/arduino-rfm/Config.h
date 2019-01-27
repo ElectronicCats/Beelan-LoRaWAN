6b53bd8b0d5afcd2961878821e567d643e303630
wero1414 <werowero.edy@gmail.com>
2019-01-27 15:06:26 -0600

    add define for 915 and subbands

--- src/arduino-rfm/Config.h
+++ src/arduino-rfm/Config.h
@@ -27,8 +27,13 @@
 
 
 // LoRaWAN freq. band
-#define AS_923
+// #define AS_923
 // #define EU_868
+#define US_915
+    //Select the subband youre working on
+    //#define SUBND_1
+    #define SUBND_6
+
 
 
 // ------------------------------------------------
