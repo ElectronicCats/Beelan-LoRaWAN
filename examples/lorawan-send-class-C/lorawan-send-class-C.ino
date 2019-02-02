3074e306dd4e14c556acaca2fddfa6e85ad8ca53
wero1414 <werowero.edy@gmail.com>
2019-02-02 12:57:39 -0600

    Add pin structure to the examples

--- examples/lorawan-send-class-C/lorawan-send-class-C.ino
+++ examples/lorawan-send-class-C/lorawan-send-class-C.ino
@@ -4,6 +4,15 @@ long interval = 10000;    // 10 s interval to send message
 long previousMillis = 0;  // will store last time message sent
 unsigned int counter = 0;     // message counter
 
+const sRFM_pins RFM_pins = {
+  .CS = SS,
+  .RST = RFM_RST,
+  .DIO0 = RFM_DIO0,
+  .DIO1 = RFM_DIO1,
+  .DIO2 = RFM_DIO2,
+  .DIO5 = RFM_DIO5,
+  };
+
 void setup() {
   // Setup loraid access
   Serial.begin(115200);
