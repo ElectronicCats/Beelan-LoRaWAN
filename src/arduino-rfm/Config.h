73ce08df3d26dc2d7cd9a22b295fe9e6edabaefa
wero1414 <werowero.edy@gmail.com>
2019-02-02 13:17:54 -0600

    Clean config.h

--- src/arduino-rfm/Config.h
+++ src/arduino-rfm/Config.h
@@ -1,31 +1,6 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-// Board definition
-//#define BOARD_DRAGINO_SHIELD
-// #define BOARD_CYTRON_SHIELD
-#define CUSTOM_BOARD
-
-// Your custom pins
-#ifdef CUSTOM_BOARD
-#if defined(ESP8266)    // for ESP based board
-    #define DIO0    2
-    #define DIO1    6
-    #define DIO5    8
-    #define DIO2    7
-    #define CS      10
-    #define RFM_RST 9        
-#else                   // for Arduino board
-    #define DIO0    RFM_DIO0
-    #define DIO1    RFM_DIO1
-    #define DIO5    RFM_DIO5
-    #define DIO2    RFM_DIO2
-    #define CS      SS
-    #define RFM_RST RFM_RST       
-#endif  
-#endif // _CUSTOM_BOARD_
-
-
 // LoRaWAN freq. band
 // #define AS_923
 // #define EU_868
@@ -34,48 +9,4 @@
     //#define SUBND_1
     #define SUBND_6
 
-
-
-// ------------------------------------------------
-// ------------- Pre-defined PINS -----------------
-// ------------------------------------------------
-/*
-#if defined(ESP8266)
-#ifdef BOARD_DRAGINO_SHIELD
-    #define DIO0    16
-    #define DIO1    2
-    #define DIO5    12
-    #define DIO2    14
-    #define CS      15
-    #define RFM_RST 13        
-#elif defined BOARD_CYTRON_SHIELD
-    #define DIO0    16
-    #define DIO1    0
-    #define DIO2    2
-    #define CS      15
-    #define RFM_RST 14       
-#elif defined BOARD_CUSTOM
-#else
-#error "Unsupported shield"
-#endif
-#else
-#ifdef BOARD_DRAGINO_SHIELD
-    #define DIO0    2
-    #define DIO1    6
-    #define DIO5    8
-    #define DIO2    7
-    #define CS      10
-    #define RFM_RST 9        
-#elif defined BOARD_CYTRON_SHIELD
-    #define DIO0    2
-    #define DIO1    5
-    #define DIO2    6
-    #define CS      10
-    #define RFM_RST 7        
-#elif defined BOARD_CUSTOM
-#else
-#error "Unsupported shield"
-#endif
-#endif
-*/
 #endif
