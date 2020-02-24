e28dc4513c285a4ec14a55fbdacbe038bd8ff097
Eduardo <werowero.edy@gmail.com>
2020-02-24 01:25:36 -0600

    add class c example

---
+++ examples/send-class-C-ABP/send-class-C-ABP.cpp
@@ -0,0 +1,76 @@
+/**
+ * Example of ABP device
+ * Authors: 
+ *        Eduardo Contreras
+ *  June 2019
+ * 
+ * This code is beerware; if you see me (or any other collaborator 
+ * member) at the local, and you've found our code helpful, 
+ * please buy us a round!
+ * Distributed as-is; no warranty is given.
+ */
+#include <lorawan.h>
+
+//ABP Credentials 
+const char *devAddr = "00000000";
+const char *nwkSKey = "00000000000000000000000000000000";
+const char *appSKey = "00000000000000000000000000000000";
+
+const unsigned long interval = 10000;    // 10 s interval to send message
+unsigned long previousMillis = 0;  // will store last time message sent
+unsigned int counter = 0;     // message counter
+
+char myStr[50];
+char outStr[255];
+byte recvStatus = 0;
+
+const sRFM_pins RFM_pins = {
+  .CS = 20,
+  .RST = 9,
+  .DIO0 = 0,
+  .DIO1 = 1,
+  .DIO2 = 2,
+  .DIO5 = 15,
+};
+
+void setup() {
+  // Setup loraid access
+  Serial.begin(115200);
+  delay(2000);
+  if(!lora.init()){
+    Serial.println("RFM95 not detected");
+    delay(5000);
+    return;
+  }
+
+  // Set LoRaWAN Class change CLASS_A or CLASS_C
+  lora.setDeviceClass(CLASS_C);
+
+  // Set Data Rate
+  lora.setDataRate(SF8BW125);
+
+  // set channel to random
+  lora.setChannel(MULTI);
+  
+  // Put ABP Key and DevAddress here
+  lora.setNwkSKey(nwkSKey);
+  lora.setAppSKey(appSKey);
+  lora.setDevAddr(devAddr);
+
+  lora.onReceive();
+}
+
+void loop() {
+  // Check interval overflow
+  if(millis() - previousMillis > interval) {
+    previousMillis = millis(); 
+
+    sprintf(myStr, "Counter-%d", counter); 
+
+    Serial.print("Sending: ");
+    Serial.println(myStr);
+    
+    lora.sendUplink(myStr, strlen(myStr), 0);
+    counter++;
+  }
+}
