01798b1ee5966b908f3cc4b8e1ab2156c319bbab
Jaimy <jjuliano77@gmail.com>
2020-03-24 13:04:31 -0500

    Changed use of SPI Transactions to cooperate with other SPI devices
    Removed SPI.beginTransaction from the LoRaWANClass::init method because it
    did not allow for other SPI devices with different settings to use the bus
    after init() was called. Instead, SPI.beginTransaction is now called at the
    top of the RFM_Write and RFM_Read methods, and SPI.endTransaction is called
    at the end of each. This allows other devices to use the bus with their own
    transaction settings.
    
    This change has been running without issue on a device for ~15 hours
    at time of commit

--- src/arduino-rfm/lorawan-arduino-rfm.cpp
+++ src/arduino-rfm/lorawan-arduino-rfm.cpp
@@ -137,7 +137,10 @@ bool LoRaWANClass::init(void)
 
     //Initialise the SPI port
     SPI.begin();
-    SPI.beginTransaction(SPISettings(4000000,MSBFIRST,SPI_MODE0));
+    
+    /*** This prevents the use of other SPI devices with different settings ***/
+    //SPI.beginTransaction(SPISettings(4000000,MSBFIRST,SPI_MODE0)); 
+
 
     //Wait until RFM module is started
     delay(50);
