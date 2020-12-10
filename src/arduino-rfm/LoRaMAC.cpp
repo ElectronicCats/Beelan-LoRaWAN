92c15483103f4d1c6a3459b348a29a913a4052e1
Eduardo <werowero.edy@gmail.com>
2020-12-10 16:40:06 -0600

    add bastwan support for RF switch

--- src/arduino-rfm/LoRaMAC.cpp
+++ src/arduino-rfm/LoRaMAC.cpp
@@ -71,12 +71,20 @@ void LORA_Cycle(sBuffer *Data_Tx, sBuffer *Data_Rx, RFM_command_t *RFM_Command, 
 	unsigned char rx1_dr = LoRa_Settings->Datarate_Tx+10;
   	//Transmit
 	if(*RFM_Command == NEW_RFM_COMMAND){
-    	//Lora send data
+		#if defined(SAMR34)
+		pinMode(RFM_SWITCH,OUTPUT);
+		digitalWrite(RFM_SWITCH,0); //Rf switch inside RAK module change to Tx
+		#elif	
+		//Lora send data
     	LORA_Send_Data(Data_Tx, Session_Data, LoRa_Settings);
 		prevTime = millis();
 		
 		// Class C open RX2 immediately after sending data
-		if(LoRa_Settings->Mote_Class == CLASS_C){	
+		if(LoRa_Settings->Mote_Class == CLASS_C){
+			#if defined(SAMR34)
+			pinMode(RFM_SWITCH,OUTPUT);
+			digitalWrite(RFM_SWITCH,1); //Rf switch inside RAK module change to Rx
+			#elif	
 			// RX2 window
 			LoRa_Settings->Channel_Rx = 0x08;    // set Rx2 channel 923.3 MHZ 
 			LoRa_Settings->Datarate_Rx = 0x08;   //set RX2 datarate 12
@@ -97,7 +105,10 @@ void LORA_Cycle(sBuffer *Data_Tx, sBuffer *Data_Rx, RFM_command_t *RFM_Command, 
 		//Return to datarate and channel for RX1
 		LoRa_Settings->Channel_Rx = rx1_ch;    // set RX1 channel 
 		LoRa_Settings->Datarate_Rx = rx1_dr;   //set RX1 datarate
-		
+		#if defined(SAMR34)
+		pinMode(RFM_SWITCH,OUTPUT);
+		digitalWrite(RFM_SWITCH,1); //Rf switch inside RAK module change to Rx
+		#elif	
 		//Receive Data RX1
 		LORA_Receive_Data(Data_Rx, Session_Data, OTAA_Data, Message_Rx, LoRa_Settings);
 		//Wait rx2 window delay 
@@ -113,7 +124,9 @@ void LORA_Cycle(sBuffer *Data_Tx, sBuffer *Data_Rx, RFM_command_t *RFM_Command, 
 		//Configure datarate and channel for RX2
 		LoRa_Settings->Channel_Rx = 0x08;    // set RX2 channel 
 		LoRa_Settings->Datarate_Rx = 0x08;   //set RX2 datarate
-		
+		#if defined(SAMR34)
+		digitalWrite(RFM_SWITCH,1); //Rf switch inside RAK module change to Rx
+		#elif	
 		//Receive Data RX2 
 		//If class A timeout will apply
 		//If class C continous Rx will happen
